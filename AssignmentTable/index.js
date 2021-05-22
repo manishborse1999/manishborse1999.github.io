aa77a64ea114b10a18206c82d0c488e85caed1d9
Manish Ravindra Borse <44261008+manishborse1999@users.noreply.github.com>
2021-05-22 16:28:22 +0530

    Add files via upload

---
+++ AssignmentTable/index.js
@@ -0,0 +1,15 @@
+
+   const btn=document.querySelector(".btn");
+   const tab=document.querySelector(".tab")
+//    const a=document.querySelectorAll(".aa");
+const cont=document.querySelector(".container");
+const a=document.getElementsByClassName("aa")
+
+   btn.addEventListener("click",()=>{
+    tab.style.backgroundColor="white";
+    tab.style.color="black";
+    cont.style.backgroundColor="black";
+    a.style.color="black"
+   })
+
+
